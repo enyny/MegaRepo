b2fd020a248af21e18b0d7b8a2fd60581f4b2490
enyny <sanicjamooboy@gmail.com>
2025-12-10 16:26:28 +0700

    Initial commit

---
+++ MegaProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+    language = "id"
+}
